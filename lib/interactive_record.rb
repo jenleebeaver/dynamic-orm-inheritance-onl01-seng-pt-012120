196f107b08ad40d491c5db31565d0aab123d79ad
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-03-31 08:16:54 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -1,6 +1,7 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
+#all of the code responsible for communicating between rb and db
 class InteractiveRecord
 
   def self.table_name
